6f590a67e3651a4b36fd42f85d7dd3ce742fea9c
QuangKadata <56817774+QuangKadata@users.noreply.github.com>
2023-02-02 16:00:16 +0700

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,109 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  constructor() { }
+  input: string = '';
+  result: string = '';
 
-  ngOnInit() {
-    const calculator = {
-      displayValue: '0',
-      firstOperand: null,
-      waitingForSecondOperand: false,
-      operator: null
-    };
+  constructor() {}
 
-    function updateDisplay() {
-      const display: any = document.querySelector('.screen-calc');
-      display.value = calculator.displayValue;
-    }
+  clickNumber(number: string) {
+    this.input = this.input + number;
+  }
+  
+  clickOperator(operator: string) {
+    this.input = this.input + operator;
+  }
 
-    function resetCalculator() {
-      calculator.displayValue = '0';
-      calculator.firstOperand = null;
-      calculator.waitingForSecondOperand = false;
-      calculator.operator = null;
-    }
-
-    updateDisplay();
-
-    const keys: any = document.querySelector('.btn-calc');
-    keys.addEventListener('click', event => {
-      const target = event.target;
-      if (!target.matches('button')) {
-        return;
-      }
-
-      if (target.classList.contains('operator')) {
-        handleOperator(target.value);
-        updateDisplay();
-        return;
-      }
-
-      if (target.classList.contains('decimal')) {
-        inputDecimal(target.value);
-        updateDisplay();
-        return;
-      }
-
-      if (target.classList.contains('clear')) {
-        resetCalculator();
-        updateDisplay();
-      }
-
-      inputDigit(target.value);
-      updateDisplay();
-      return;
-    });
-
-    function inputDigit(digit) {
-      const { displayValue, waitingForSecondOperand } = calculator;
-
-      if (waitingForSecondOperand === true) {
-        calculator.displayValue = digit;
-        calculator.waitingForSecondOperand = false;
-      } else {
-        calculator.displayValue =
-          displayValue === '0' ? digit : displayValue + digit;
-      }
-    }
-
-    function inputDecimal(dot) {
-      if (calculator.waitingForSecondOperand === true) {
-        return;
-      }
-      if (!calculator.displayValue.includes(dot)) {
-        calculator.displayValue += dot;
-      }
-    }
-    const performCalculation = {
-      '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
-      '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
-      '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
-      '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
-      '=': (firstOperand, secondOperand) => secondOperand
-    };
-
-    function handleOperator(nextOperator) {
-      const { firstOperand, displayValue, operator } = calculator;
-      const inputValue = parseFloat(displayValue);
-
-      if (operator && calculator.waitingForSecondOperand) {
-        calculator.operator = nextOperator;
-        return;
-      }
-
-      if (firstOperand == null) {
-        calculator.firstOperand = inputValue;
-      } else if (operator) {
-        const currentValue = firstOperand || 0;
-        const result = performCalculation[operator](currentValue, inputValue);
-        calculator.displayValue = String(result);
-        calculator.firstOperand = result;
-      }
-      calculator.waitingForSecondOperand = true;
-      calculator.operator = nextOperator;
+  clearInput() {
+    if (this.input != '') {
+      this.input = this.input.substring(0, this.input.length - 1);
     }
   }
+  clickAllClear() {
+    this.input = '';
+    this.result = '';
+  }
+
+  getResultAnswer() {
+    this.input = this.result;
+    if (this.input == '0'){
+      this.input = '';
+    }
+  }
+
+  ngOnInit(): void {}
 }
